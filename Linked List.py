f3149eed8ba9a6cd9eeb51f12d3e9858423a10d5
sureshsigera@gmail.com <96765726140676d61696c2e636f6da5666f726365c2>
2018-09-19 11:49:05 -0400

    Linked List initial commit.

---
+++ Linked List.py
@@ -0,0 +1,30 @@
+class Node(object):
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def add(self, item):
+        node = Node(item)
+        if self.head is None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+
+    def display(self):
+        current = self.head
+        while current is not None:
+            print(current.item)
+            current = current.next
+
+
+linkedlist = LinkedList()
+items = ['Apple', 'Elderberry', 'Carambola', '‎Bilberry', 'Pineapple', 'Banana', 'Peach', 'Kiwifruit']
+for i in range(len(items)):
+    linkedlist.add(items[i])
+linkedlist.display()
